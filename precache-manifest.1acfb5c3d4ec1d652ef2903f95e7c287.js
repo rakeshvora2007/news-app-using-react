32a64f4d4d4ffcd14cc72be70deb654ddf065b49
Rakesh Jain <rakeshvora2007@gmail.com>
2020-09-21 21:40:07 -0400

    Updates

---
+++ precache-manifest.1acfb5c3d4ec1d652ef2903f95e7c287.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3b932be72e8ba1f1c9618ac1dc72368",
+    "url": "/news-app-using-react/index.html"
+  },
+  {
+    "revision": "69f6f897244d572666da",
+    "url": "/news-app-using-react/static/css/main.999c1c15.chunk.css"
+  },
+  {
+    "revision": "638c05141fb6e84c3359",
+    "url": "/news-app-using-react/static/js/2.d321877b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/news-app-using-react/static/js/2.d321877b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69f6f897244d572666da",
+    "url": "/news-app-using-react/static/js/main.bc131730.chunk.js"
+  },
+  {
+    "revision": "a60ee279194936e5ec93",
+    "url": "/news-app-using-react/static/js/runtime-main.7b8de6f5.js"
+  }
+]);
